0020f2d4acb90bd218a7f2b583fcf07906e3cb8b
SantiSL5 <santiagosolerllin@gmail.com>
2021-11-25 21:54:04 +0100

    repaired errors workflow2

--- Hello.js
+++ Hello.js
@@ -1,7 +1,7 @@
-let express = require("express");
-let app = express();
+const express = require("express");
+const app = express();
 
-app.get('/', function (req, res) {
+app.get("/", function (req, res) {
   res.end("Hello World!");
 });
 
